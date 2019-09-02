d913df71c378cf8735519af35b78b7b327b09aff
Billy Kim <billyhkim.dev@gmail.com>
2019-09-02 11:10:57 -0700

    added loop to send out messages to all clients

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const ws = new WebSocket.Server({
   'port': 8888
 });
 
+const connections = [];
+
 ws.on('connection', conn => {
-  console.log('GOT a connection!');
+  connections.push(conn);
+  console.log('GOT a connection!', 'count: ', connections.length);
   conn.on('message', message => {
     console.log('got a message', message);
-    conn.send(`echo: ${message}`);
+    for (var connIndex = connections.length - 1; connIndex >= 0; connIndex--) {
+      connections[connIndex].send(message);
+    }
   })
   conn.on('close', () => {
     console.log('connection closed');

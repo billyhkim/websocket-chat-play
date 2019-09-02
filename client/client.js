560fc3510a446cbd2bf454a4ebab2116abcf934a
Billy Kim <billyhkim.dev@gmail.com>
2019-09-02 10:47:16 -0700

    able to message in loop send and receive on client

--- client/client.js
+++ client/client.js
@@ -1,3 +1,5 @@
+$(document).ready(startApp);
+
 const ws = new WebSocket('ws://localhost:8888');
 
 ws.addEventListener('open', event => {
@@ -7,4 +9,19 @@ ws.addEventListener('open', event => {
 
 ws.addEventListener('message', event => {
   console.log('got a message', event.data);
+  displayMessage(event.data);
 });
+
+function startApp() {
+  $("#send").click(sendMessage);
+}
+
+function sendMessage() {
+  const message = $("#outbound-message").val();
+  ws.send(message);
+  $("#outbound-message").val('');
+}
+
+function displayMessage(message) {
+  $("#display").html(message + "<br>" + $("#display").html());
+}

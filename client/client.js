d64a27e0551754dffb05617aa51e6c16ff2ef71b
Billy Kim <billyhkim.dev@gmail.com>
2019-09-03 11:27:10 -0700

    completed multiple user send and receive functionality

--- client/client.js
+++ client/client.js
@@ -4,24 +4,48 @@ const ws = new WebSocket('ws://localhost:8888');
 
 ws.addEventListener('open', event => {
   console.log('made a connection!');
-  ws.send('greetings!');
+  sendPacketMessage('someone has joined the chat');
 });
 
 ws.addEventListener('message', event => {
   console.log('got a message', event.data);
-  displayMessage(event.data);
+  const data = JSON.parse(event.data);
+  const message = data.message;
+  const from = data.from;
+  displayMessage(from, message);
 });
 
 function startApp() {
   $("#send").click(sendMessage);
 }
 
+function sendPacketMessage(message) {
+  const packetToSend = JSON.stringify({
+    name: $("#name").val(),
+    message
+  });
+  ws.send(packetToSend);
+}
+
 function sendMessage() {
   const message = $("#outbound-message").val();
-  ws.send(message);
+
+  sendPacketMessage(message);
   $("#outbound-message").val('');
 }
 
-function displayMessage(message) {
-  $("#display").html(message + "<br>" + $("#display").html());
+function displayMessage(name, message) {
+  var messageContainer = $("<div>", {
+    class: 'container-message'
+  });
+  var name = $("<div>", {
+    text: name,
+    class: "name"
+  });
+  var message = $("<div>", {
+    text: message,
+    class: 'message'
+  })
+  messageContainer.append(name, message);
+  $("#display").prepend(messageContainer);
 }
--- server.js
+++ server.js
@@ -9,10 +9,16 @@ const connections = [];
 ws.on('connection', conn => {
   connections.push(conn);
   console.log('GOT a connection!', 'count: ', connections.length);
-  conn.on('message', message => {
-    console.log('got a message', message);
+  conn.on('message', dataString => {
+    const data = JSON.parse(dataString);
+    const message = data.message;
+    const name = data.name;
+    console.log('got a message', data);
     for (var connIndex = connections.length - 1; connIndex >= 0; connIndex--) {
-      connections[connIndex].send(message);
+      connections[connIndex].send(JSON.stringify({
+        from: name,
+        message
+      }));
     }
   })
   conn.on('close', () => {
